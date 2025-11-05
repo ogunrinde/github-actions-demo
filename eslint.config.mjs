2f8c5a8c1e7c73900ebb852b3dcc3215f90096d1
Omotayo Ogunrinde <omotayo@steer.global>
2025-11-04 21:40:31 -0500

    updated eslint and added prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,33 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+
+    plugins: { js, prettierPlugin }, 
+    extends: [js.configs.recommended, prettierConfig],
+    rules: {
+      // Integrate Prettier (throws error if formatting is wrong)
+      "prettier/prettier": "error",
+
+      // Your previous ESLint rules
+      "no-unused-vars": ["warn"],
+      semi: ["error", "always"],
+      indent: ["error", 2],
+    },
+    languageOptions: { globals: globals.browser } 
+  },
 ]);
